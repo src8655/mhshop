f43b05233306c533acc963b203b32cd01abaad19
src8655 <src8655@naver.com>
2019-07-10 14:52:10 +0900

    MemberController 테스트

--- mhshop/src/main/java/com/cafe24/mhshop/controller/api/MemberController.java
+++ mhshop/src/main/java/com/cafe24/mhshop/controller/api/MemberController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cafe24.mhshop.dto.JSONResult;
 import com.cafe24.mhshop.service.MemberService;
 import com.cafe24.mhshop.vo.MemberVo;
 
@@ -34,21 +35,23 @@ public class MemberController {
 	
 	@RequestMapping(value = "/join_form", method = RequestMethod.GET)
 	@ApiOperation(value = "[회원약관과 회원가입 입력 페이지]", notes = "회원가입 요청 API")
-	public Map<String, Object> join_form() {
+	public JSONResult join_form() {
 		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("result", "success");
-		map.put("message", null);
-		map.put("data", "join/join_form");
 		
-		return map;
+		// JSON 리턴 생성
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		dataMap.put("forward", "join/join_form");
+		return JSONResult.success(dataMap);
 	}
 	
 	
-	
+
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "id", value = "아이디", paramType = "path", required = true, defaultValue = "")
+	})
 	@RequestMapping(value = "/join_idcheck/{id}", method = RequestMethod.GET)
 	@ApiOperation(value = "회원ID 중복여부 확인", notes = "회원ID 중복확인 API")
-	public Map<String, Object> idcheck(
+	public JSONResult idcheck(
 			@PathVariable(value = "id") String id
 			) {
 		
@@ -57,24 +60,26 @@ public class MemberController {
 		
 		// Service에 요청
 		
-		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("result", "success");
-		map.put("message", null);
-		map.put("data", true);	// isExist
-		
-		return map;
+		return JSONResult.success(false);
 	}
 	
 	
-	
-	@RequestMapping(value = "/join_post", method = RequestMethod.POST)
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "id", value = "아이디", paramType = "query", required = true, defaultValue = ""),
+		@ApiImplicitParam(name = "password", value = "비밀번호", paramType = "query", required = true, defaultValue = ""),
+		@ApiImplicitParam(name = "name", value = "이름", paramType = "query", required = true, defaultValue = ""),
+		@ApiImplicitParam(name = "phone", value = "연락처", paramType = "query", required = true, defaultValue = ""),
+		@ApiImplicitParam(name = "email", value = "이메일", paramType = "query", required = true, defaultValue = ""),
+		@ApiImplicitParam(name = "zipcode", value = "우편번호", paramType = "query", required = true, defaultValue = ""),
+		@ApiImplicitParam(name = "addr", value = "주소", paramType = "query", required = true, defaultValue = "")
+	})
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
 	@ApiOperation(value = "회원을 DB에 등록", notes = "회원등록 API")
-	public Map<String, Object> join_post(
+	public Map<String, Object> join(
 			@RequestParam(value = "id", required = true, defaultValue = "") String id,
 			@RequestParam(value = "password", required = true, defaultValue = "") String password,
 			@RequestParam(value = "name", required = true, defaultValue = "") String name,
-			@RequestParam(value = "phones", required = true, defaultValue = "") String[] phones,
+			@RequestParam(value = "phone", required = true, defaultValue = "") String phone,
 			@RequestParam(value = "email", required = true, defaultValue = "") String email,
 			@RequestParam(value = "zipcode", required = true, defaultValue = "") String zipcode,
 			@RequestParam(value = "addr", required = true, defaultValue = "") String addr
@@ -89,7 +94,7 @@ public class MemberController {
 		mvo.setId(id);
 		mvo.setPassword(password);
 		mvo.setName(name);
-		mvo.setPhone(phones[0] + "-" + phones[1] + "-" + phones[2]);
+		mvo.setPhone(phone);
 		mvo.setEmail(email);
 		mvo.setZipcode(zipcode);
 		mvo.setAddr(addr);
@@ -111,14 +116,12 @@ public class MemberController {
 	@RequestMapping(value = "/join_result", method = RequestMethod.GET)
 	@ApiOperation(value = "[회원가입 결과 페이지]", notes = "회원가입 결과 API")
 	@ResponseBody
-	public Map<String, Object> join_result() {
+	public JSONResult join_result() {
 
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("result", "success");
-		map.put("message", null);
-		map.put("data", "join/join_result");
-		
-		return map;
+		// JSON 리턴 생성
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		dataMap.put("forward", "join/join_result");
+		return JSONResult.success(dataMap);
 	}
 	
 	
@@ -127,19 +130,20 @@ public class MemberController {
 	@RequestMapping(value = "/login_form", method = RequestMethod.GET)
 	@ApiOperation(value = "[로그인 페이지]", notes = "로그인 요청 API")
 	@ResponseBody
-	public Map<String, Object> login_form() {
-
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("result", "success");
-		map.put("message", null);
-		map.put("data", "login/login_form");
+	public JSONResult login_form() {
 		
-		return map;
+		// JSON 리턴 생성
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		dataMap.put("forward", "login/login_form");
+		return JSONResult.success(dataMap);
 	}
 	
 	
 	
-	
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "id", value = "아이디", paramType = "query", required = true, defaultValue = ""),
+		@ApiImplicitParam(name = "password", value = "비밀번호", paramType = "query", required = true, defaultValue = "")
+	})
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ApiOperation(value = "회원 로그인", notes = "회원 로그인 API")
 	public Map<String, Object> login(
--- mhshop/src/test/java/com/cafe24/mhshop/controller/api/MemberControllerTest.java
+++ mhshop/src/test/java/com/cafe24/mhshop/controller/api/MemberControllerTest.java
@@ -4,18 +4,23 @@ import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
@@ -33,73 +38,173 @@ public class MemberControllerTest {
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 	
+	@BeforeClass
+	public static void classSetup() {
+		// DB 초기화
+	}
+	
 	@Before
 	public void setup() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 		
-		// db초기화
 	}
 	
-	// 회원가입 컨트롤러 테스트
+	
+
+	// [회원약관, 회원가입 페이지]
 	@Test
-	public void testFetchJoin() throws Exception {
+	public void testAJoinForm() throws Exception {
 		
-		// [회원약관, 회원가입 페이지]
-		mockMvc.perform(get("/api/member/join_form")).andExpect(status().isOk()).andDo(print());
+		ResultActions resultActions = mockMvc.perform(get("/api/member/join_form").contentType(MediaType.APPLICATION_JSON));
 		
-		// 아이디 중복확인
-		mockMvc.perform(get("/api/member/join_idcheck/{id}", "test_id")).andExpect(status().isOk()).andDo(print());
+		// 응답이 200 인지
+		// 결과가 성공햇는지
+		// 포워드할 페이지를 리턴하는지
+		resultActions
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.result", is("success")))
+		.andExpect(jsonPath("$.data.forward", is("join/join_form")));
 		
-		// 회원 등록
-		mockMvc.perform(post("/api/member/join_post")
+	}
+	
+	
+	// 아이디 중복확인
+	@Test
+	public void testBJoinIdCheck() throws Exception {
+
+		ResultActions resultActions = mockMvc.perform(get("/api/member/join_idcheck/{id}", "test_id").contentType(MediaType.APPLICATION_JSON));
+		
+		// 응답이 200 인지
+		// 결과가 성공햇는지
+		// 존재하지 않는지
+		resultActions
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.result", is("success")))
+		.andExpect(jsonPath("$.data", is(false)));
+	}
+
+	// 회원 등록
+	@Test
+	public void testCJoin() throws Exception {
+		
+		ResultActions resultActions = mockMvc.perform(post("/api/member/join")
 				.param("id", "test_id")
 				.param("password", "test_password")
 				.param("name", "test_name")
-				.param("phones", "test_1,test_2,test_3")
+				.param("phones", "010-0000-0000")
 				.param("email", "test_email")
 				.param("zipcode", "test_zipcode")
-				.param("addr", "test_addr"))
-		.andExpect(status().isOk()).andDo(print());
+				.param("addr", "test_addr")
+				.contentType(MediaType.APPLICATION_JSON));
 		
-
-		// [회원가입 결과 페이지]
-		mockMvc.perform(get("/api/member/join_result")).andExpect(status().isOk()).andDo(print());
+		// 응답이 200 인지
+		// 결과가 성공햇는지
+		// DB의 사용자 정보 결과 확인
+		// 리다이렉트할 페이지를 리턴하는지
+		resultActions
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.result", is("success")))
 		
+		.andExpect(jsonPath("$.data.memberVo.id", is("test_id")))
+		.andExpect(jsonPath("$.data.memberVo.password", is("test_password")))
+		.andExpect(jsonPath("$.data.memberVo.name", is("test_name")))
+		.andExpect(jsonPath("$.data.memberVo.phones", is("010-0000-0000")))
+		.andExpect(jsonPath("$.data.memberVo.email", is("test_email")))
+		.andExpect(jsonPath("$.data.memberVo.zipcode", is("test_zipcode")))
+		.andExpect(jsonPath("$.data.memberVo.addr", is("test_addr")))
+		.andExpect(jsonPath("$.data.memberVo.role", is("USER")))
+		
+		.andExpect(jsonPath("$.data.redirect", is("/join_result")));
 		
 	}
 	
 	
-
-	// 로그인 컨트롤러 테스트
+	// [회원가입 결과 페이지]
 	@Test
-	public void testFetchLogin() throws Exception {
+	public void testDJoinResult() throws Exception {
 		
-		
-		// [로그인 페이지]
-		mockMvc.perform(get("/api/member/login_form")).andExpect(status().isOk());
+		ResultActions resultActions = mockMvc.perform(get("/api/member/join_result").contentType(MediaType.APPLICATION_JSON));
 
+		// 응답이 200 인지
+		// 결과가 성공햇는지
+		// 포워드할 페이지를 리턴하는지
+		resultActions
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.result", is("success")))
+		.andExpect(jsonPath("$.data.forward", is("join/join_result")));
 		
+	}
 
-		// 회원 로그인
-		mockMvc.perform(post("/api/member/login")
+	// [로그인 페이지]
+	@Test
+	public void testELoginForm() throws Exception {
+		
+		ResultActions resultActions = mockMvc.perform(get("/api/member/login_form").contentType(MediaType.APPLICATION_JSON));
+
+		// 응답이 200 인지
+		// 결과가 성공햇는지
+		// 포워드할 페이지를 리턴하는지
+		resultActions
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.result", is("success")))
+		.andExpect(jsonPath("$.data.forward", is("login/login_form")));
+	}
+
+	// 회원 로그인
+	@Test
+	public void testFLogin() throws Exception {
+
+		ResultActions resultActions = mockMvc.perform(post("/api/member/login")
 				.param("member_id", "test_id")
-				.param("password", "test_password"))
-		.andExpect(status().isOk()).andDo(print());
-		
-		
-		
+				.param("password", "test_password")
+				.contentType(MediaType.APPLICATION_JSON));
 
-		// 회원 로그아웃
-		mockMvc.perform(post("/api/member/logout")).andExpect(status().isOk()).andDo(print());
+		// 응답이 200 인지
+		// 결과가 성공햇는지
+		// 로그인 성공 여부
+		// 로그인된 정보
+		// 리다이렉트할 페이지를 리턴하는지
+		resultActions
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.result", is("success")))
+		.andExpect(jsonPath("$.data.result", is(true)))
+		
+		.andExpect(jsonPath("$.data.memberVo.id", is("test_id")))
+		.andExpect(jsonPath("$.data.memberVo.password", is("test_password")))
+		.andExpect(jsonPath("$.data.memberVo.name", is("test_name")))
+		.andExpect(jsonPath("$.data.memberVo.phones", is("010-0000-0000")))
+		.andExpect(jsonPath("$.data.memberVo.email", is("test_email")))
+		.andExpect(jsonPath("$.data.memberVo.zipcode", is("test_zipcode")))
+		.andExpect(jsonPath("$.data.memberVo.addr", is("test_addr")))
+		.andExpect(jsonPath("$.data.memberVo.role", is("USER")))
+		
+		.andExpect(jsonPath("$.data.redirect", is("/")));
+	}
+	
+
+	// 회원 로그아웃
+	@Test
+	public void testGLogout() throws Exception {
+
+		ResultActions resultActions = mockMvc.perform(post("/api/member/logout").contentType(MediaType.APPLICATION_JSON));
+		
+		// 응답이 200 인지
+		// 결과가 성공햇는지
+		// 로그아웃 성공 여부
+		// 리다이렉트할 페이지를 리턴하는지
+		resultActions
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.result", is("success")))
+		.andExpect(jsonPath("$.data.result", is(true)))
+		.andExpect(jsonPath("$.data.redirect", is("/")));
 		
 	}
 	
 	
-	
-	
-	@After
-	public void finish() {
-		
-		// db초기화
+	@AfterClass
+	public static void finish() {
+		// DB 초기화
 	}
+	
+	
 }
