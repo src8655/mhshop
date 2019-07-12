6fe0584196c6b8724f977c2a787b7e60f6a69fa3
src8655 <src8655@naver.com>
2019-07-12 19:27:51 +0900

    no message

--- mhshop/src/main/java/com/cafe24/mhshop/controller/api/MemberController.java
+++ mhshop/src/main/java/com/cafe24/mhshop/controller/api/MemberController.java
@@ -11,6 +11,7 @@ import javax.websocket.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
