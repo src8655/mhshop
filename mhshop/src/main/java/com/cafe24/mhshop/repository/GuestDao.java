36cebc7d52d2ef9cfda45e1dc3e2dc22c0a2d633
src8655 <src8655@naver.com>
2019-07-23 09:43:51 +0900

    spring boot 프로젝트만 남기기

--- mhshop/src/main/java/com/cafe24/mhshop/repository/GuestDao.java
+++
@@ -1,9 +0,0 @@
-package com.cafe24.mhshop.repository;
-
-import com.cafe24.mhshop.vo.GuestVo;
-
-public interface GuestDao {
-
-	GuestVo selectOne(GuestVo guestVo);		// 비회원 상세
-
-}
--- mhshop/src/main/java/com/cafe24/mhshop/service/GuestService.java
+++
@@ -1,9 +0,0 @@
-package com.cafe24.mhshop.service;
-
-import com.cafe24.mhshop.vo.GuestVo;
-
-public interface GuestService {
-
-	GuestVo getByOrdersNo(String ordersNo);		// 비회원 상세
-
-}
